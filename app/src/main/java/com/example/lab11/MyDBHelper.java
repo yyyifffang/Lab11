89d7eac53f42e0b9f74e41d53311f55d6342bc9a
yyyifffang <Fragrant115@gmail.com>
2022-12-16 11:02:26 +0800

    first commit

---
+++ app/src/main/java/com/example/lab11/MyDBHelper.java
@@ -0,0 +1,28 @@
+package com.example.lab11;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    private static  final String name = "mdatabase.db";
+    private static final int version =1;
+
+    MyDBHelper(Context context){
+        super(context,name,null,version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE myTable(book text PRIMARY KEY,price integer NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS myTable");
+        onCreate(db);
+    }
+}
+
+
